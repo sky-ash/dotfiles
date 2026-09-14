41493f9787dd30eb5010a8d6ff9961d8ef2fe6c9
sky-ash <sky-ash-dev@proton.me>
2026-09-14 18:02:45 +0200

    peripheral changes

--- dotfiles/.config/hypr/hyprland/input.lua
+++ dotfiles/.config/hypr/hyprland/input.lua
@@ -6,6 +6,6 @@ hl.config({
     input = {
         kb_layout  = "us,de",
         follow_mouse = 2,
-        sensitivity = -1 , -- -1.0 - 1.0, 0 means no modification.
+        sensitivity = -0.5 , -- -1.0 - 1.0, 0 means no modification.
     },
 })
--- dotfiles/.config/hypr/hyprland/monitors.lua
+++ dotfiles/.config/hypr/hyprland/monitors.lua
@@ -4,16 +4,19 @@
 
 -- ULTRAWIDE (21:9) MONITOR CENTER
 hl.monitor({
-    output   = "HDMI-A-1",
-    mode     = "3440x1440", -- ? "3440x1440@144"
+    --output   = "HDMI-A-1",
+    output   = "DP-1",
+    mode     = "3440x1440@99.99", -- ? "3440x1440@144"
     position = "0x0",
     scale    = "1",
 })
 
--- 16:9 MONITOR TO THE LEFT
+-- 16:9 MONITOR TO THE... 
 hl.monitor({
-    output   = "HDMI-A-2",
-    mode     = "1920x1080", -- ? "1920x1080@60"
-    position = "3440x0",
+    --output   = "HDMI-A-2",
+    output   = "DP-2",
+    mode     = "1920x1080@59.96", -- ? "1920x1080@60"
+    position = "3440x0",    -- RIGHT
+    --position = "-1920x0",   -- LEFT
     scale    = "1",
 })
